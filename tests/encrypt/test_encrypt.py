a055ef932b4297eec55bfcecb4fc498bf0f1c48f
Nina Palma <ninapalmadev@gmail.com>
2023-11-28 13:42:24 -0300

    REQ 2: Criptografia de inversões

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message('message', 'stg')
+
+    assert encrypt_message('message', 10) == 'egassem'
+    assert encrypt_message('message', 3) == 'sem_egas'
+    assert encrypt_message('message', 4) == 'ega_ssem'
