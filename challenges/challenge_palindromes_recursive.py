34d58269167f63e9368337b08a0492f837be7736
Nina Palma <ninapalmadev@gmail.com>
2023-11-28 13:49:44 -0300

    REQ 3: Palíndromos (Recursividade)

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word is None or len(word) == 0:
+        return False
+    if low_index > high_index:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
