3d230ce108427f57b3c3b6b2543097131b609a98
Nina Palma <ninapalmadev@gmail.com>
2023-11-28 13:24:05 -0300

    REQ 1: Algoritmo de busca

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+
+    try:
+        for permanence, target in permanence_period:
+            if permanence <= target_time <= target:
+                counter += 1
+    except (TypeError):
+        return None
+    return counter
